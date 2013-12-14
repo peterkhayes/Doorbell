18635aa102a58d8ce887ad1db64f80c197565507
Hack Reactor Students <github@hackreactor.com>
2013-12-14 14:20:37 -0800

    comments for easyAjax

--- spec/easyAjax.js
+++ spec/easyAjax.js
@@ -3,30 +3,29 @@
 var http = require('http');
 
 var ajax = function(options){
+  //If user tries to pass non-string, automatically stringifies it
   if (!typeof (options.data === 'string')){
     options.data = JSON.stringify(options.data);
   }
+  // Default method and contentType
   options.contentType = options.contentType || 'application/JSON';
   options.method = options.method || 'GET';
-  options.callback = function(response){
-    if (options.success && response.statusCode < 400){
-      options.success();
-    }
-    if (options.error && response.statusCode > 399){
-      options.error(response);
-    }
-    if (options.then){
-      options.then();
-    }
+
+  //allowing success and error functionality (unless already has callback)
+  options.callback = options.callback || function(response){
+    if (options.success && response.statusCode < 400) options.success();
+    if (options.error && response.statusCode > 399) options.error(response);
+    if (options.then) options.then();
   };
 
+  //Making the request
   var request = http.request({
     hostname: options.host,
     path: options.path,
     headers: {'Content-Type': options.contentType},
     method: options.method
   }, options.callback);
-  request.write(options.data);
+  if (options.data) request.write(options.data);
   request.end();
 };
 
