cb352743be6f0efeaa1a0dda20547a2f5eaa973a
Peter Hayes <peter.k.hayes@gmail.com>
2013-12-13 20:52:11 -0800

    fixed template syntax

--- app_modules/templates.js
+++ app_modules/templates.js
@@ -1,22 +1,27 @@
+/*
+  Templates for response messages.
+  If messages are sent out as texts, subject is simply the first sentence.
+*/
+
 var temperature = require('./temperature');
 
-exports.templates = {
-  ring: function(data) {
-    return {
-      subject: data.name + " is at the door!",
-      text: "It's currently " + temperature.getTemperature() + " outside."
-    };
-  },
-  unring: function(data) {
-    return {
-      subject: "Never mind.",
-      text: data.name + " found his or her way inside safely."
-    };
-  },
-  help: function() {
-    return {
-      subject: 'How to doorbell:',
-      text: "YOUR NAME: ringBell(); 'who': whosThere(); 'cancel': unringBell(); 'exit': leave();"
-    };
-  }
+exports.ring = function(data) {
+  return {
+    subject: data.name + " is at the door!",
+    text: "It's currently " + temperature.getTemperature() + " outside."
+  };
+};
+
+exports.unring = function(data) {
+  return {
+    subject: "Never mind.",
+    text: data.name + " found his or her way inside safely."
+  };
+};
+
+exports.help = function() {
+  return {
+    subject: 'How to doorbell:',
+    text: "YOUR NAME: ringBell(); 'who': whosThere(); 'cancel': unringBell(); 'exit': leave();"
+  };
 };
